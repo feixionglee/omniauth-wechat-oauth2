0235da0816556a31c82c5b10ccd4a5f7c50c2d89
lifeixiong <feixiongli@gmail.com>
2015-01-09 00:42:37 +0800

    get wechat user info

--- lib/omniauth/strategies/wechat.rb
+++ lib/omniauth/strategies/wechat.rb
@@ -36,7 +36,7 @@ module OmniAuth
       end
 
       def request_phase
-        params = client.auth_code.authorize_params.merge(redirect_uri: callback_url).merge(authorize_params)
+        params = client.auth_code.authorize_params.merge(redirect_uri: callback_url).merge(scope: "snsapi_userinfo")
         params["appid"] = params.delete("client_id")
         redirect client.authorize_url(params)
       end
